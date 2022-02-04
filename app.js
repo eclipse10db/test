dccfe2e51cea927c1c8cf8c7a9753ff033c470c3
eclipse10db <98991466+eclipse10db@users.noreply.github.com>
2022-02-03 18:02:08 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = 'ec2-54-165-25-207.compute-1.amazonaws.com';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
